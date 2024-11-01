bbe68aa1c7c4786ecf7ee4e23b196004f2300934
Emre Gül <emregl246@outlook.com.tr>
2024-11-01 11:04:59 +0300

    module01 updated

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -5,7 +5,10 @@ int	main(int ac, char **av)
 	Vessel ves;
 
 	if (ac != 2)
+	{
 		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+		return (0);
+	}
 	ves.complain(av[1]);
 	return (0);
 }
