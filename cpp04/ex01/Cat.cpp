f05de5bf85fe189cceae13daf390e4109e521dd0
Emre Gül <144537902+emre-mr246@users.noreply.github.com>
2025-01-06 22:04:52 +0000

    deep copy mechanism improved for cat class

--- cpp04/ex01/Cat.cpp
+++ cpp04/ex01/Cat.cpp
@@ -3,13 +3,13 @@
 Cat::Cat(void)
 {
 	this->_type = "Cat";
-	std::cout << "Default constructor called for " << this->_type << std::endl;
 	this->_brain = new Brain();
 	if (!this->_brain)
 	{
 		std::cerr << "Brain allocation failed for Cat!" << std::endl;
 		return ;
 	}
+	std::cout << "Default constructor called for " << this->_type << std::endl;
 }
 
 Cat::~Cat()
@@ -21,14 +21,21 @@ Cat::~Cat()
 
 Cat::Cat(const Cat &src) : Animal()
 {
-	this->_type = src._type;
-	this->_brain = new Brain(*src._brain);
-	std::cout << "Copy constructor called for " << this->_type << std::endl;
+    this->_type = src._type;
+    if (src._brain)
+        this->_brain = new Brain(*src._brain);
+	else
+        this->_brain = NULL;
+    std::cout << "Copy constructor called for " << src._type << std::endl;
 }
 
 Cat &Cat::operator=(const Cat &src)
 {
+    if (this == &src)
+    	return (*this);
 	this->_type = src._type;
+	if (this->_brain)
+		delete(this->_brain);
 	this->_brain = new Brain(*src._brain);
 	std::cout << "Copy assignment operator called for " << this->_type << std::endl;
 	return (*this);
@@ -42,4 +49,9 @@ void Cat::makeSound(void) const
 void Cat::newIdea(unsigned int i, std::string idea)
 {
     this->_brain->setIdea(i, idea);
+}
+
+const std::string Cat::getIdea(unsigned int i) const
+{
+	return (this->_brain->getIdea(i));
 }
--- cpp04/ex01/Cat.hpp
+++ cpp04/ex01/Cat.hpp
@@ -16,6 +16,7 @@ public:
 	Cat &operator=(const Cat &src);
 	void makeSound(void) const;
 	void newIdea(unsigned int i, std::string idea);
+	const std::string getIdea(unsigned int i) const;
 
 private:
 	Brain *_brain;
