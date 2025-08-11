9356309c0d49594b6ba5b916f4b9df46ba4edd86
Emre Gül <emregl246@outlook.com.tr>
2025-08-11 13:44:12 +0300

    Add Makefile, main.cpp, and whatever.hpp for initial project setup

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emgul <emgul@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 12:38:23 by emgul             #+#    #+#             */
+/*   Updated: 2025/07/28 12:38:23 by emgul            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main(void)
+{
+    int a = 2;
+    int b = 3;
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    std::cout << "=============================" << std::endl;
+
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+
+    return (0);
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emgul <emgul@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 12:38:26 by emgul             #+#    #+#             */
+/*   Updated: 2025/08/09 00:00:00 by emgul            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T> void swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T> const T &min(const T &a, const T &b)
+{
+    return ((b < a) ? b : a);
+}
+
+template <typename T> const T &max(const T &a, const T &b)
+{
+    return ((a < b) ? b : a);
+}
+
+#endif
