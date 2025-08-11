d92126d411ed60e0e91407ed20ef2cc02595d9e7
Emre Gül <emregl246@outlook.com.tr>
2025-08-11 20:11:03 +0300

    feat: Add main.cpp, iter.hpp, and Makefile for new iteration functionality

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emgul <emgul@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 12:38:26 by emgul             #+#    #+#             */
+/*   Updated: 2025/08/09 00:00:00 by emgul            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T> void iter(T *array, size_t length, void (*func)(T &))
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < length)
+    {
+        func(array[i]);
+        i++;
+    }
+}
+
+#endif
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emgul <emgul@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 12:38:23 by emgul             #+#    #+#             */
+/*   Updated: 2025/07/28 12:38:23 by emgul            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+template <typename T> void print(T &element)
+{
+    std::cout << element << std::endl;
+}
+
+int main(void)
+{
+    double tab[] = {4.0, 2.0};
+    iter(tab, 2, print);
+
+    std::string strTab[] = {"Hi", "there!"};
+    iter(strTab, 2, print);
+
+    int intTab[] = {42, 246};
+    iter(intTab, 2, print);
+
+    return (0);
+}
